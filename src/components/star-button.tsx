f08f3b9e76c4e01d69aaf8867d15823e6fe27c19
Mohan Reddy <madhiremohanreddy@gmail.com>
2025-08-12 08:06:29 +0000

    the button stokeer color postion is not good in light theme

--- src/components/star-button.tsx
+++ src/components/star-button.tsx
@@ -11,7 +11,7 @@ export default function StarButton({ href, text, className }: StarButtonProps) {
   return (
     <Link href={href} passHref>
       <button type="button" className={cn("relative group", className)}>
-        <div className="absolute -inset-0.5 bg-gradient-to-r from-primary/70 to-secondary/70 rounded-full blur-md opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+        <div className="absolute -inset-0.5 bg-gradient-to-r from-primary to-destructive rounded-full blur-md opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
         <div className="relative px-7 py-4 bg-background rounded-full leading-none flex items-center">
           <span className="text-foreground uppercase tracking-widest text-sm font-medium">{text}</span>
         </div>
