79289fd5680fa98ca5c7be789291bf512cbae04c
Mohan Reddy <madhiremohanreddy@gmail.com>
2025-09-03 12:50:56 +0000

    adjust the select box layout to minimal and make the proper scorling fea

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -70,7 +71,7 @@ SelectScrollDownButton.displayName =
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
+>(({ className, children, position = "popper", side = "bottom", ...props }, ref) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
       ref={ref}
@@ -81,12 +82,13 @@ const SelectContent = React.forwardRef<
         className
       )}
       position={position}
+      side={side}
       {...props}
     >
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
-          "p-1 w-full",
+          "p-1",
           position === "popper" &&
             "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
         )}
