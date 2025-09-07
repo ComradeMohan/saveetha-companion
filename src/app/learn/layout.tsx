939a52a9ac785c05397c250d6b139e11580b4aa3
Mohan Reddy <madhiremohanreddy@gmail.com>
2025-09-07 16:21:12 +0000

    maintian seaprar scrolbar for raodmap whne i scorll the navbar contenst

--- src/app/learn/layout.tsx
+++ src/app/learn/layout.tsx
@@ -56,11 +56,11 @@ export default function LearnLayout({
   }
   
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <LearnSidebar />
       <div className="flex flex-col">
         <AdminHeader />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40 overflow-auto">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40 overflow-y-auto">
             {children}
         </main>
       </div>
