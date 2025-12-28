103f62a8620017c14004ff2c7ae9e8bcf44e067f
Mohan Reddy <madhiremohanreddy@gmail.com>
2025-12-28 15:12:26 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

---
+++ src/next.config.ts
@@ -0,0 +1,52 @@
+
+import type {NextConfig} from 'next';
+require('dotenv').config()
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd112y698adiu2z.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'remotive.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse'],
+  },
+};
+
+export default nextConfig;
