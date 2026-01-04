221eaa88701c127d81c47c73d399420438d6d58a
Mohan Reddy <madhiremohanreddy@gmail.com>
2026-01-04 16:55:34 +0000

    You’re hitting a **classic Next.js App Router + Client Component trap**,

--- src/app/robot/page.tsx
+++ src/app/robot/page.tsx
@@ -1,17 +1,9 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const Spline = dynamic(() => import('@splinetool/react-spline/next'), {
-  ssr: false,
-});
+import RobotSpline from '@/components/RobotSpline';
 
 export default function RobotPage() {
   return (
     <main className="h-screen w-full">
-      <Spline
-        scene="https://prod.spline.design/0o4S2x7ky3TKB5za/scene.splinecode" 
-      />
+      <RobotSpline />
     </main>
   );
 }
