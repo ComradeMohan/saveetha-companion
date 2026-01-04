eaf94f5fa94a0cb97527ce779a2f43ed8451dba7
Mohan Reddy <madhiremohanreddy@gmail.com>
2026-01-04 16:49:47 +0000

    import Spline from '@splinetool/react-spline/next';
    export default func

---
+++ src/app/robot/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Spline from '@splinetool/react-spline/next';
+
+export default function RobotPage() {
+  return (
+    <main className="h-screen w-full">
+      <Spline
+        scene="https://prod.spline.design/0o4S2x7ky3TKB5za/scene.splinecode" 
+      />
+    </main>
+  );
+}
