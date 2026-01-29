8686e74d78a2b6160676c5d0ef60dac5d233c515
Mohan Reddy <madhiremohanreddy@gmail.com>
2026-01-29 09:54:04 +0000

    updathe mani {   "theme_color": "#0b0f14", //cahnge teh colour bsed on w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,8 +72,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#f5f7fb" },
-    { media: "(prefers-color-scheme: dark)", color: "#0b0f14" },
+    { media: "(prefers-color-scheme: light)", color: "#FFFCFA" },
+    { media: "(prefers-color-scheme: dark)", color: "#03081F" },
   ],
 };
 
